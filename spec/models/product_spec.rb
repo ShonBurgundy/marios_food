95fb46239c4ba5f154651a591beb9aee499cb3d4
shonburgundy <seanbeloussow@yahoo.com>
2020-05-29 10:08:14 -0700

    add failing test for titleize callback on product name

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,4 +3,8 @@ require 'rails_helper'
 describe Product do
   it {should have_many(:reviews)}
 
+  it("titleizes the name of a product") do
+    product = Product.create({name: "apple", cost: 2, country: "usa"})
+    expect(product.name()).to(eq("Apple"))
+  end
 end
