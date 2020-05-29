aa68459f591d6e10f27a75821e204a6f43be02e7
shonburgundy <seanbeloussow@yahoo.com>
2020-05-29 12:14:47 -0700

    pass content max length validation test

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,5 +5,6 @@ class Review < ApplicationRecord
   validates :content, presence: true
   validates :rating, presence: true
   validates_numericality_of :rating, only_integer: true
+  validates_length_of :content, maximum: 250
 
 end
