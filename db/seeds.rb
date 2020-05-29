97298afcb9c943e8714ac82439552ba6ed820946
shonburgundy <seanbeloussow@yahoo.com>
2020-05-29 15:55:22 -0700

    add views layouts/troubleshooting faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ Product.destroy_all
 
 50.times do |index|
   Product.create!(name: Faker::Food.dish,
-                          cost: Faker::Number.decimal(l_digits: 2)
+                          cost: Faker::Number.decimal(l_digits: 2),
                           country: Faker::WorldCup.team)
   end
 
@@ -18,10 +18,15 @@ Review.destroy_all
 
 250.times do |index|
   Review.create!(author: Faker::FunnyName.two_word_name,
-                          content: Faker::Lorem.sentence(55, false, 0).chop,
+                          content: Faker::Lorem.sentence(word_count: 55).chop,
                           rating: Faker::Number.between(from: 1, to: 5))
   end
 
 
 p "Created #{Product.count} products!"
 p "Created #{Review.count} reviews!"
+
+# Keyword arguments: word_count, supplemental, random_words_to_add
+# The 'random_words_to_add' argument increases the sentence's word count by a random value within (0..random_words_to_add).
+# To specify an exact word count for a sentence, set word_count to the number you want and random_words_to_add equal to 0.
+# By default, sentences will not have any random words added (and will all be exactly 4 words long)
