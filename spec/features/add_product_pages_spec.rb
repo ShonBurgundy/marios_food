dc08166654dc1d09e694797f6ffea8dce010afe1
shonburgundy <seanbeloussow@yahoo.com>
2020-05-29 19:09:59 -0700

    pass int error int test for product add

---
+++ spec/features/add_product_pages_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "the add a product process" do
+  it "adds a new product" do
+    visit root_path
+    click_link 'Add a new product'
+    fill_in 'Name', :with => 'Oreos'
+    fill_in 'Cost', :with => '4'
+    fill_in 'Country', :with => 'usa'
+    click_on 'Create Product'
+    expect(page).to have_content 'Oreos'
+  end
+
+  it "gives an error when inputs are incomplete" do
+    visit new_product_path
+    click_on 'Create Product'
+    expect(page).to have_content "Name can't be blank"
+  end
+end
