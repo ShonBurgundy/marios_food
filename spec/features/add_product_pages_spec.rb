efca47a01b4c043939ef361e777b93656d4236b3
shonburgundy <seanbeloussow@yahoo.com>
2020-05-29 19:12:37 -0700

    pass int error int test for cost, country product add

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -16,4 +16,16 @@ describe "the add a product process" do
     click_on 'Create Product'
     expect(page).to have_content "Name can't be blank"
   end
+
+  it "gives an error when inputs are incomplete" do
+    visit new_product_path
+    click_on 'Create Product'
+    expect(page).to have_content "Cost can't be blank"
+  end
+
+  it "gives an error when inputs are incomplete" do
+    visit new_product_path
+    click_on 'Create Product'
+    expect(page).to have_content "Country can't be blank"
+  end
 end
