bfb67ff8f6a79afe31906b823cbccf56c3fdc8ed
shonburgundy <seanbeloussow@yahoo.com>
2020-06-05 08:47:19 -0700

    add encrypt_pasword method

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,11 @@ class User < ApplicationRecord
     :presence => true, 
     confirmation: { case_sensitive: false }
   before_save :encrypt_password
+
+  def encrypt_password
+    self.password_salt = BCrypt::Engine.generate_salt
+    self.password_hash = BCrypt::Engine.hash_secret(password,password_salt)
+  end
+
   
 end
