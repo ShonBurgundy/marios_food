76a4106f76f57127e87a50d8a6950555195954d0
shonburgundy <seanbeloussow@yahoo.com>
2020-06-05 08:56:25 -0700

    add user & session routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
   resources :products do
     resources :reviews
   end
+
+    # USER ROUTES 
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
+    
+    # SESSION ROUTES 
+    get '/signin' => 'sessions#new'
+    post '/signin' => 'sessions#create'
+    get '/signout' => 'sessions#destroy'
+    
 end
